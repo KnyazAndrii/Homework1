e377d02a69c5503f2cf16b66408bf23bc217d59b
KnyazAndrii <andrijko.zagaruk@gmail.com>
2022-06-28 18:08:58 +0300

    Changed methods by example from class work

--- Homework1/Program.cs
+++ Homework1/Program.cs
@@ -4,114 +4,68 @@ namespace Homework1
 {
     class Program
     {
-        static void Task1()
+        static int ConvertHoursToMinutes(int numHours)
         {
-            Console.Write("Enter number of hours: ");
-            int NumHours = Convert.ToInt32(Console.ReadLine());
-            int NumMinutes = NumHours * 60;
-
-            Console.WriteLine("Result: " + NumMinutes);
-        }
-        static void Task2()
-        {
-            Console.Write("Enter A value: ");
-            double A = Convert.ToDouble(Console.ReadLine());
-            Console.Write("Enter B value: ");
-            double B = Convert.ToDouble(Console.ReadLine());
-            double result = (5 * A + B * B) / (B - A);
-
-            Console.WriteLine("Result: " + result);
+            return numHours * 60;
         }
 
-        static void Task3()
+        static double CalculateEquation(double a, double b)
         {
-            Console.Write("Enter string A: ");
-            string A = Console.ReadLine();
-            Console.Write("Enter string B: ");
-            string B = Console.ReadLine();
-            Console.WriteLine("A - " + A + "  B - " + B);
-
-            string swap = A;
-            A = B;
-            B = swap;
-
-            Console.WriteLine("Swapped A - " + A + "  Swapped B - " + B);
+            return (5 * a + b * b) / (b - a);
         }
 
-        static void Task4()
+        static void Swap(ref string a, ref string b)
         {
-            Console.Write("Enter A value: ");
-            int A = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Enter B value: ");
-            int B = Convert.ToInt32(Console.ReadLine());
-
-            int dividing = A / B;
-            Console.WriteLine("Dividing: " + dividing);
-
-            int remainder = A % B;
-            Console.WriteLine("Remainder: " + remainder);
+            string swap = a;
+            a = b;
+            b = swap;
         }
 
-        static void Task5()
+        static int Divide(int a, int b)
         {
-            Console.Write("Enter A value: ");
-            double A = Convert.ToDouble(Console.ReadLine());
-            Console.Write("Enter B value: ");
-            double B = Convert.ToDouble(Console.ReadLine());
-            Console.Write("Enter C value: ");
-            double C = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Your linear equation: " + A + " * X + " + B + " = " + C);
+            return a / b;
+        }    
 
-            double X = (C - B) / A;
-            Console.WriteLine("X = " + X);
+        static int DivideRemind(int a, int b)
+        { 
+            return a % b;
         }
 
-        static void Task6()
+        static double CalculateLinearEquation(double a, double b, double c)
         {
-            Console.Write("Enter X1 value: ");
-            double X1 = Convert.ToDouble(Console.ReadLine());
-            Console.Write("Enter Y1 value: ");
-            double Y1 = Convert.ToDouble(Console.ReadLine());
-            Console.Write("Enter X2 value: ");
-            double X2 = Convert.ToDouble(Console.ReadLine());
-            Console.Write("Enter Y2 value: ");
-            double Y2 = Convert.ToDouble(Console.ReadLine());
-            
-            double A = (Y2 - Y1)/(X2 - X1);
-            Console.WriteLine("Equation of a straight line: " + Y1 + " = " + A + " * " + X1 + " + B");
-
-            double B = Y1 - A * X1;
-            Console.WriteLine("B = " + B);
+            return (c - b) / a;
         }
 
-        static void Task7()
+        static string MakeStraightLineEquation(double x1, double y1, double x2, double y2)
+        {            
+            double a = (y2 - y1)/(x2 - x1);
+            return ("Equation of a straight line: " + y1 + " = " + a + " * " + x1 + " + B");
+        }
+
+        static int CalculateSumOfDigits(int n)
         {
-            Console.Write("Enter N value: ");
-            int N = Convert.ToInt32(Console.ReadLine());
 
-            int FirstDigit = N / 10;
-            int SeccondDigit = N % 10;
-            int SumDigits = FirstDigit + SeccondDigit;
-
-            Console.WriteLine("The sum of the digits of N: " + SumDigits);
+            int FirstDigit = n / 10;
+            int SeccondDigit = n % 10;
+            return FirstDigit + SeccondDigit;
         }
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Task 1:");
-            Task1();
-            Console.WriteLine("Task 2:");
-            Task2();
-            Console.WriteLine("Task 3:");
-            Task3();
-            Console.WriteLine("Task 4:");
-            Task4();
-            Console.WriteLine("Task 5:");
-            Task5();
-            Console.WriteLine("Task 6:");
-            Task6();
-            Console.WriteLine("Task 7:");
-            Task7();
+            string a = "Hello ";
+            string b = "World ";
+            Console.WriteLine("Task 1: " + ConvertHoursToMinutes(4));
+
+            Console.WriteLine("Task 2: " + CalculateEquation(3, 5));
+
+            Swap(ref a, ref b);
+            Console.WriteLine("Task 3: " + a + b);
+
+            Console.WriteLine("Task 4: " + Divide(6, 4) + 
+                Environment.NewLine + DivideRemind(6, 4));
+            Console.WriteLine("Task 5: " + CalculateLinearEquation(2, 3, 7));
+            Console.WriteLine("Task 6: " + MakeStraightLineEquation(5, 2, 8, 10));
+            Console.WriteLine("Task 7: " + CalculateSumOfDigits(25));
         }
     }
 }
