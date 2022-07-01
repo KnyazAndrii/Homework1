dbd974a36fc87345ddf5ed51af9374070de3c0f4
KnyazAndrii <andrijko.zagaruk@gmail.com>
2022-07-01 19:15:27 +0300

    Removed the empty line 47; Changed return in task 6.

--- Homework1/Program.cs
+++ Homework1/Program.cs
@@ -39,12 +39,11 @@ namespace Homework1
         static string MakeStraightLineEquation(double x1, double y1, double x2, double y2)
         {            
             double a = (y2 - y1)/(x2 - x1);
-            return ("Equation of a straight line: " + y1 + " = " + a + " * " + x1 + " + B");
+            return a.ToString();
         }
 
         static int CalculateSumOfDigits(int n)
         {
-
             int FirstDigit = n / 10;
             int SeccondDigit = n % 10;
             return FirstDigit + SeccondDigit;
@@ -54,17 +53,21 @@ namespace Homework1
         {
             string a = "Hello ";
             string b = "World ";
-            Console.WriteLine("Task 1: " + ConvertHoursToMinutes(4));
+            double x1 = 5;
+            double y1 = 2;
+            double x2 = 8;
+            double y2 = 10;
 
+            Console.WriteLine("Task 1: " + ConvertHoursToMinutes(4));
             Console.WriteLine("Task 2: " + CalculateEquation(3, 5));
 
             Swap(ref a, ref b);
-            Console.WriteLine("Task 3: " + a + b);
 
+            Console.WriteLine("Task 3: " + a + b);
             Console.WriteLine("Task 4: " + Divide(6, 4) + 
                 Environment.NewLine + DivideRemind(6, 4));
             Console.WriteLine("Task 5: " + CalculateLinearEquation(2, 3, 7));
-            Console.WriteLine("Task 6: " + MakeStraightLineEquation(5, 2, 8, 10));
+            Console.WriteLine("Task 6: " + "Equation of a straight line: " + y1 + " = " + MakeStraightLineEquation(x1, y1, x2, y2) + " * " + x1 + " + B");
             Console.WriteLine("Task 7: " + CalculateSumOfDigits(25));
         }
     }
